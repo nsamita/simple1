3f061211748956ae7f99fefa96769998cc2576ef
fafon41 <supanitung41@gmail.com>
2018-03-09 17:38:42 +0700

    Simple2

---
+++ lab8_1/lab8_1/Simple2.py
@@ -0,0 +1,36 @@
+import sys
+from lab8_1 import Simple_drawing_window
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+class Simple_drawing_window2(Simple_drawing_window):
+    def __init__(self):
+        return super().__init__(self)
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,0))
+        p.drawPolygon(
+            QPoint(70,100), QPoint(100,110),
+            QPoint(130,100), QPoint(100,150),
+            )
+        p.setPen(QColor(255,127,0))
+        p.setBrush(QColor(255,127,0))
+        p.drawPie(50,150,100,100,0,180*16)
+        p.drawPolygon(
+            QPoint(50,200), QPoint(150,200), QPoint(100,400),
+            )
+        p.drawPixmap(QRect(200,100,320,320), self.rabbit)
+        p.end()
+def main():
+    app = QApplication(sys.argv)
+    w = Simple_drawing_window()
+    w.show()
+        
+    return app.exec_()
+    
+if __name__ == "__main__":
+    sys.exit(main()) 
